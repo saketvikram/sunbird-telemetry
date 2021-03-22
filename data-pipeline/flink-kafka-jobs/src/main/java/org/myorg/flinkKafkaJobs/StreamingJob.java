0e50185d7ed421fc3e0addefd1a49401410b75da
Saket Singh <saket.singh@tarento.com>
2021-03-22 11:35:24 +0530

    flink kafka jobs added

---
+++ data-pipeline/flink-kafka-jobs/src/main/java/org/myorg/flinkKafkaJobs/StreamingJob.java
@@ -0,0 +1,43 @@
+
+package org.myorg.flinkKafkaJobs;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * Skeleton for a Flink Streaming Job.
+ * <p>To package your application into a JAR file for execution, run
+ * 'mvn clean package' on the command line.
+ *
+ * <p>If you change the name of the main class (with the public static void main(String[] args))
+ * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
+ */
+public class StreamingJob {
+
+	public static void main(String[] args) throws Exception {
+		// set up the streaming execution environment
+		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+		/*
+		 * Here, you can start creating your execution plan for Flink.
+		 *
+		 * Start with getting some data from the environment, like
+		 * 	env.readTextFile(textPath);
+		 *
+		 * then, transform the resulting DataStream<String> using operations
+		 * like
+		 * 	.filter()
+		 * 	.flatMap()
+		 * 	.join()
+		 * 	.coGroup()
+		 *
+		 * and many more.
+		 * Have a look at the programming guide for the Java API:
+		 *
+		 * https://flink.apache.org/docs/latest/apis/streaming/index.html
+		 *
+		 */
+
+		// execute program
+		env.execute("Flink Streaming Java API Skeleton");
+	}
+}
